c7af93f9e06a6f5b38d1b16a1e876db186948319
FedericoDM <fd.molina@outlook.com>
2024-07-17 19:41:38 +0000

    Completing parse function

--- parserator_web/views.py
+++ parserator_web/views.py
@@ -6,6 +6,7 @@ from rest_framework.renderers import JSONRenderer
 from rest_framework.exceptions import ParseError
 
 
+
 class Home(TemplateView):
     template_name = 'parserator_web/index.html'
 
@@ -14,11 +15,43 @@ class AddressParse(APIView):
     renderer_classes = [JSONRenderer]
 
     def get(self, request):
+        """
+        Extracts the address from the get request
+        """
         # TODO: Flesh out this method to parse an address string using the
         # parse() method and return the parsed components to the frontend.
-        return Response({})
+        
+        address = request.GET("address", "")
+        parsed_address, address_type = self.parse(address)
+        
+        return Response({"input_string": address,
+                         "address_components": parsed_address,
+                         "address_type": address_type })
 
     def parse(self, address):
+        """
+        Uses the usaddress package to parse the address
+        """
         # TODO: Implement this method to return the parsed components of a
         # given address using usaddress: https://github.com/datamade/usaddress
+
+        if not address:
+            raise ParseError("Address can not be empty")
+        
+        try:
+            parsed_address = usaddress.parse(address)
+            address_type = usaddress.tag(address)
+
+        except Exception as error:
+            print(f"Error parsing address: {error}")
+            return {}, "ERROR"
+
+        address_components = {}
+        
+        for element in parsed_address:
+            address_components[element[1]] = element[0]
+
+        address_type = address_type[-1]
+        
+
         return address_components, address_type
